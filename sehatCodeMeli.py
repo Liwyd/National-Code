346a4293245749f85333f6b5fcb5d9d63e16371c
gray <82864026+cinzentoo@users.noreply.github.com>
2023-08-01 00:19:05 +0330

    Add files via upload

---
+++ sehatCodeMeli.py
@@ -0,0 +1,16 @@
+code = str(input('[Enter]: '))
+flag = 10
+sum = 0
+for i in code[:-1]:
+    sum += int(int(i)*flag)
+    flag -= 1
+
+if sum%11 >= 2:
+    true_one = (code[:-1]+str(11-(sum%11)))
+else:
+    true_one = (code[:-1]+str(sum%11))
+
+if true_one == code:
+    print('True')
+else:
+    print("False")
